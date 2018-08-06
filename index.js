362093c8d0df300a3e0b43a1f3b8c87f28a7965f
YoungDi <u9951062@gmail.com>
2018-08-06 17:49:13 +0800

    init commit

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
-'use strict'
-const Service = require('./lib/clients/v2/Service')
-const SignatureValidator = require('./lib/clients/v2/SignatureValidator')
+const express = require('express');
+const line = require('@line/bot-sdk');
+const bodyParser = require('body-parser');
+const config = {
+  channelAccessToken: 'wkVMfW4q27td/y53WrLM7e7Ooy+JfH4WOSrYaksn9KgL+tZoQDhui6rL0sIb7GNVU1Eq4j/yr9Wpj+5Hgf5UkmFHatmy6jk9w//ngIAdbb1VB6yu6QFU4JPzQueJax0OhYfv196HV+HfU5QCzFXi4wdB04t89/1O/w1cDnyilFU=',
+  channelSecret: '227738d158bdbee5e1aa58f19cb99e13'
+};
+const JSONParseError = require('@line/bot-sdk').JSONParseError
+const SignatureValidationFailed = require('@line/bot-sdk').SignatureValidationFailed
 
-module.exports = {
+const app = express();
 
-  init (options) {
-    this.client = new Service(options)
-    this.validator = new SignatureValidator(options)
+app.use('/webhook', line.middleware(config))
+app.use(bodyParser.json())
+
+app.use((err, req, res, next) => {
+  if (err instanceof SignatureValidationFailed) {
+    res.status(401).send(err.signature)
+    return
+  } else if (err instanceof JSONParseError) {
+    res.status(400).send(err.raw)
+    return
+  }
+  next(err) // will throw default 500
+})
+
+app.post('/webhook', (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => console.log(err));
+});
+
+const client = new line.Client(config);
+function handleEvent(event) {
+  console.log(event);
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null);
   }
 
+  return client.replyMessage(event.replyToken, {
+    type: 'text',
+    text: event.message.text
+  });
 }
+
+app.listen(3000);
